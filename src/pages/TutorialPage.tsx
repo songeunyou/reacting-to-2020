8356278874e25ebbec6b5a04d1107fd404ab829e
song <songeun.you7@gmail.com>
2020-09-25 02:24:13 -0600

    Styled instructions and code editor
    - Dark theme for instructions and code editor
    - Created callout component

--- src/pages/TutorialPage.tsx
+++ src/pages/TutorialPage.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import '../css/App.css';
 
+interface CalloutProps {
+    emoji: string;
+    text: string;
+}
+
+function Callout({emoji, text}: CalloutProps) {
+    return (
+        <div className="callout">
+            <p>{emoji}</p>
+            <p>{text}</p>
+        </div>
+    );
+}
+
 function TutorialPage() {
   return (
     <div className="tutorial-page">
         <div className="info">
             <div className="instructions">
                 FILLER INSTRUCTIONS: My all-time favorite oven baked chicken breasts recipe!  It never fails me, thanks to this simple 5-step method that is easy to make and customize with your favorite seasonings.
+                <Callout emoji="💩" text="This is a callout, for little texts you might wanna add about things!"/>
             </div>
             <div className="code-editor">
             </div>
