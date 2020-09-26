c12851d91f65409794f9f5079051a5f3aced0633
Su Min Kim <kims622@gmail.com>
2020-09-25 20:38:22 -0400

    Add module and question structures. Add functionality to update UI depending on module and question id.

--- src/pages/TutorialPage.tsx
+++ src/pages/TutorialPage.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../css/App.css';
 import Callout from '../components/Callout';
+import {allModules} from '../objects/Modules';
 
 interface TutorialProps {
-    id: string;
+    id: number;
 }
 
-// TutorialPage needs to be rewritten so that depending on which module you are on (different id),
-// Info and visuals are swapped out
-
 function TutorialPage({id}: TutorialProps) {
+  const module = allModules[id-1];
+  const nextModuleIndex = Number(id) + 1;
+  const numberOfQuestions = module.questions.length;
+  const [question, setQuestion] = useState(0);
+
+  useEffect(() => {
+    setQuestion(0);
+  }, [id]);
+
   return (
     <div className="tutorial-page">
         <div className="info">
             <div className="instructions">
-                FILLER INSTRUCTIONS: My all-time favorite oven baked chicken breasts recipe!  It never fails me, thanks to this simple 5-step method that is easy to make and customize with your favorite seasonings.
+                {module.questions[question].explanation}
+                {/* FILLER INSTRUCTIONS: My all-time favorite oven baked chicken breasts recipe!  It never fails me, thanks to this simple 5-step method that is easy to make and customize with your favorite seasonings. */}
                 <Callout emoji="💩" text="This is a callout, for little texts you might wanna add about things!"/>
+                {
+                question < numberOfQuestions-1 
+                ? <button onClick={() => setQuestion(question + 1)}>Next</button> 
+                : <div className="start-btn">
+                      <Link to={`/tutorial/${nextModuleIndex}`} key={nextModuleIndex}>
+                          <p>Next Module</p>
+                      </Link>
+                  </div>
+                }
             </div>
             <div className="code-editor">
             </div>
