9f64585c5df79b543504f2f8d1ea1e523596348a
Su Min Kim <kims622@gmail.com>
2020-09-26 10:54:37 -0400

    Add route to success page when module 3 finishes

--- src/pages/TutorialPage.tsx
+++ src/pages/TutorialPage.tsx
@@ -12,7 +12,6 @@ interface TutorialProps {
 function TutorialPage({id}: TutorialProps) {
   const module = allModules[id-1];
   const nextModuleIndex = Number(id) + 1;
-  const numberOfQuestions = module.questions.length;
   const [question, setQuestion] = useState(0);
 
   useEffect(() => {
@@ -27,13 +26,19 @@ function TutorialPage({id}: TutorialProps) {
                 {/* FILLER INSTRUCTIONS: My all-time favorite oven baked chicken breasts recipe!  It never fails me, thanks to this simple 5-step method that is easy to make and customize with your favorite seasonings. */}
                 <Callout emoji="💩" text="This is a callout, for little texts you might wanna add about things!"/>
                 {
-                question < numberOfQuestions-1 
+                question < module.questions.length-1 
                 ? <button onClick={() => setQuestion(question + 1)}>Next</button> 
-                : <div className="start-btn">
-                      <Link to={`/tutorial/${nextModuleIndex}`} key={nextModuleIndex}>
-                          <p>Next Module</p>
-                      </Link>
-                  </div>
+                : id == 3 
+                  ? <div className="start-btn">
+                        <Link to={`/success}`}>
+                            <p>Finish</p>
+                        </Link>
+                    </div>
+                  : <div className="start-btn">
+                        <Link to={`/tutorial/${nextModuleIndex}`} key={nextModuleIndex}>
+                            <p>Next Module</p>
+                        </Link>
+                    </div>
                 }
             </div>
             <div className="code-editor">
